9d5725358999790c8b705b22df9ca83e19022354
Imran Kheraj <ikheraj17@gmail.com>
2020-04-20 18:13:26 -0500

    dependcies installed, mongoDB schema defined

--- database/index.js
+++ database/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  owner: String,
+  repo: String,
+  Repo_url: String,
+  Forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
